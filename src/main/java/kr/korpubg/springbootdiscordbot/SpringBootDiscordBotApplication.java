8b1dbd83a8ec6d20130b00009afa9876034c5d81
0n1dev <all_mind@naver.com>
2021-06-15 20:39:00 +0900

    init : Spring Boot Discord Bot
    - Spring Boot Discord Bot Initialize

---
+++ src/main/java/kr/korpubg/springbootdiscordbot/SpringBootDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package kr.korpubg.springbootdiscordbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDiscordBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDiscordBotApplication.class, args);
+    }
+
+}
